8535b710ecb19c7e98239ca16b7a35e548e79ffa
b3east1 <rhylendelarosa@gmail.com>
2023-12-13 12:05:52 -0800

    wokring on coin and zombie

---
+++ final mini game/Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI coinCountText;
+    public int coins = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        coinCountText.text = "Coins: " + coins.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coins++;
+        coinCountText.text = "Coins; " + coins.ToString();
+
+    }
+}
